d9c876eb746e593e0511bc456c6fe5ad0c690a5c
Yuva <gnuyuva@gmail.com>
2010-08-17 19:05:35 +0530

    added gitconfig

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 #
 # installs all the files to $HOME directory.
 
-FILES="bash_profile bashrc dircolors emacs"
+FILES="bash_profile bashrc dircolors emacs \
+gitconfig"
+
+# just create links in home directory!
 for f in $FILES; do
    ln -s $(pwd)/$f $HOME/.$f
 done
