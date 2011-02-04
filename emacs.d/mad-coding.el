61e1ba93545eccaa2de043281d560154e084bba2
Yuva <gnuyuva@gmail.com>
2011-02-05 01:11:30 +0530

    added lots of modes for ruby and rails ...

--- emacs.d/mad-coding.el
+++ emacs.d/mad-coding.el
@@ -9,7 +9,23 @@
 (setq-default perl-indent-level 3)
 
 
+;; add yasnippets.
+(add-to-list 'load-path (concat top-dir "/yasnippet"))
+(require 'yasnippet)
+(yas/initialize)
+(setq yas/snippet-dirs (concat top-dir "/yasnippet/snippets"))
+(yas/load-directory yas/snippet-dirs)
+
+;; add auto-completions everywhere.
+(add-to-list 'load-path (concat top-dir "/auto-complete"))
+(require 'auto-complete)
+(add-to-list 'ac-dictionary-directories (concat top-dir "/auto-complete/dict"))
+(require 'auto-complete-config)
+(ac-config-default)
+
+
 ;; i love this mode!
+;; for c and cpp
 (defun yk-cpp-mode ()
   "C/C++ mode for me!"
   (interactive)
@@ -17,6 +33,10 @@
   (setq indent-tabs-mode nil)
   (c-set-style "stroustrup")
   (setq c-basic-offset 3))
+
+(add-to-list 'auto-mode-alist
+             '("\\.\\(cpp\\|hpp\\|c\\|h\\|inl\\)$" . yk-cpp-mode))
+
 
 ;; add latest ruby-mode
 (add-to-list 'load-path (concat top-dir "/ruby-mode"))
@@ -26,26 +46,46 @@
             (require 'ruby-electric)
             (ruby-electric-mode t)))
 
-;; add yasnippets.
-(add-to-list 'load-path (concat top-dir "/yasnippet"))
-(require 'yasnippet)
-(yas/initialize)
-(setq yas/snippet-dirs (concat top-dir "/yasnippet/snippets"))
-(yas/load-directory yas/snippet-dirs)
+(add-to-list 'auto-mode-alist
+             '("\\(\\.\\(rb\\|rake\\|gemspec\\)\\|Rakefile\\)$" . ruby-mode))
 
-;; add auto-complete mode.
-(add-to-list 'load-path (concat top-dir "/auto-complete"))
-(require 'auto-complete)
-(add-to-list 'ac-dictionary-directories (concat top-dir "/auto-complete/dict"))
-(require 'auto-complete-config)
-(ac-config-default)
 
-;; append the list.
-;; regexp for .cpp, .hpp, .c, .h or .inl
-(setq auto-mode-alist
-      (append
-       '(("\\.\\(cpp\\|hpp\\|c\\|h\\|inl\\)$" . yk-cpp-mode))
-       '(("Rakefile$" . ruby-mode))
-       auto-mode-alist))
+;; add haml and sass modes
+(add-to-list 'load-path (concat top-dir "/haml-mode"))
+(add-to-list 'load-path (concat top-dir "/sass-mode"))
+(require 'haml-mode)
+(require 'sass-mode)
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . sass-mode))
+
+;; add javascript2 mode
+(add-to-list 'load-path (concat top-dir "/js2-mode"))
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+
+;; add yaml mode
+(add-to-list 'load-path (concat top-dir "/yaml-mode"))
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+
+;; add cucumber-mode
+(add-to-list 'load-path (concat top-dir "/cucumber-mode"))
+(require 'feature-mode)
+(yas/load-directory (concat top-dir "/cucumber-mode/snippets"))
+(add-to-list 'auto-mode-alist '("\\.feature$" . feature-mode))
+
+
+;; setup rinari
+(add-to-list 'load-path (concat top-dir "/rinari"))
+(require 'rinari)
+
+
+;; add rhtml mode {checking this stuff}
+(add-to-list 'load-path (concat top-dir "/rhtml-mode"))
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+          (lambda () (rinari-launch)))
 
 (provide 'mad-coding)
