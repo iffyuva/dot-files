228ee05c4478aa3916a1f09df134235883bbaf8a
Yuva <yuva@codemancers.com>
2012-07-02 10:42:35 +0530

    add el-get for better handling of packages

--- emacs.d/new-elpa.el
+++ emacs.d/new-elpa.el
@@ -39,4 +39,14 @@
             (return (concat package-user-dir "/" dir))))))
 
 
+;; el-get - very useful!
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (goto-char (point-max))
+     (eval-print-last-sexp))))
+
 (provide 'new-elpa)
